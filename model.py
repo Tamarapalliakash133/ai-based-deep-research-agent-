82c39362a9edd4b5d4bc7cd1d6ab66df8f8e994c
Tamarapalliakash133 <akashtamarapalli43@gmail.com>
2026-04-19 12:38:24 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,13 +1,16 @@
 from langchain_openai import ChatOpenAI
-import os 
-from dotenv import load_dotenv
-load_dotenv()
+import os
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 if not OPENAI_API_KEY:
-    print("There is some interrupt in api key sorry for inconivence")
+    raise ValueError("OPENAI_API_KEY is not set")
+
 
 def my_model():
-    model = ChatOpenAI(model = "gpt-4.1-mini",temperature = 0.3)
+    model = ChatOpenAI(
+        model="gpt-4.1-mini",
+        temperature=0.3,
+        api_key=OPENAI_API_KEY
+    )
     return model
